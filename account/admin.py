38d4544ebfe872b89ce5097fa915b4fda173c6ef
meisampw1384 <mpwryamehr585@gmail.com>
2024-07-20 12:23:56 +0330

    feat : add profile admin

--- account/admin.py
+++ account/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 
 
+from .models import Profile
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    fields = ('user','avatar')
+    list_display = ['user','avatar','phone_number']
+    
+
 
 
